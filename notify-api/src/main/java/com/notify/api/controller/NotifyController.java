8b004c199e9a6629607e9d6d3ed64d656ad1cb5f
vlad2 <vlad2005646@gmail.com>
2026-07-27 17:03:36 +0400

    added swagger documentation

--- notify-api/src/main/java/com/notify/api/controller/NotifyController.java
+++ notify-api/src/main/java/com/notify/api/controller/NotifyController.java
@@ -4,6 +4,9 @@ import com.notify.api.dto.RequestEmailDTO;
 import com.notify.api.dto.RequestPushDTO;
 import com.notify.api.dto.RequestSmsDTO;
 import com.notify.api.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,21 +21,45 @@ import java.util.UUID;
 public class NotifyController {
     private final NotificationService notificationService;
 
+    @Operation(
+            summary = "Send a email notification to external APIs",
+            description = "Returns a void"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Notification sent"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
     @PostMapping("/email")
-    public ResponseEntity<RequestEmailDTO> sendNotify(@Valid @RequestBody RequestEmailDTO emailDTO){
+    public ResponseEntity<Void> sendNotify(@Valid @RequestBody RequestEmailDTO emailDTO){
         notificationService.create(emailDTO, UUID.randomUUID().toString());
-        return ResponseEntity.status(HttpStatus.CREATED).body(emailDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(
+            summary = "Send a sms notification to external APIs",
+            description = "Returns a void"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Notification sent"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
     @PostMapping("/sms")
-    public ResponseEntity<RequestSmsDTO> sendNotify(@Valid @RequestBody RequestSmsDTO smsDTO){
+    public ResponseEntity<Void> sendNotify(@Valid @RequestBody RequestSmsDTO smsDTO){
         notificationService.create(smsDTO, UUID.randomUUID().toString());
-        return ResponseEntity.status(HttpStatus.CREATED).body(smsDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(
+            summary = "Send a push notification to external APIs",
+            description = "Returns a void"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Notification sent"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
     @PostMapping("/push")
-    public ResponseEntity<RequestPushDTO> sendNotify(@Valid @RequestBody RequestPushDTO pushDTO){
+    public ResponseEntity<Void> sendNotify(@Valid @RequestBody RequestPushDTO pushDTO){
         notificationService.create(pushDTO, UUID.randomUUID().toString());
-        return ResponseEntity.status(HttpStatus.CREATED).body(pushDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
